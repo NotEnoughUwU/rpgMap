500c0b7a2c09f59ed26f9a652e5bb5c35a76f766
Martel <W0460435@campus.nscc.ca>
2022-11-09 16:23:50 -0400

    adding player movement and collision

--- rpgMap/Program.cs
+++ rpgMap/Program.cs
@@ -29,14 +29,21 @@ namespace rpgMap
         // ~ = water
         // * = trees
 
-        static int mapScale = 6;
+        static int mapScale;
+
+        static int playerX;
+        static int playerY;
+        static int subX;
+        static int subY;
+
+        static char player = '☻';
         
         static void Main(string[] args)
         {
-            //SetScale();
-            DrawMap();
-            DrawBorder();
-            
+            ClearInputBuffer();
+            SetScale();
+            InitPlayer();
+            InputLoop();
             Console.ReadKey(true);
         }
 
@@ -47,28 +54,39 @@ namespace rpgMap
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine();
-            Console.Write("INPUT MAP SCALE INTEGER: ");
+            Console.Write("INPUT MAP SCALE INTEGER:");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(" ");
+            Console.ForegroundColor = ConsoleColor.White;
 
-            try
-            {
-                char mapScaleChar = Console.ReadKey().KeyChar;
-                Console.WriteLine(mapScale);
-                Console.ReadKey(true);
-            }
-            catch (Exception)
-            {
-                SetScale();
-            }
+            char mapScaleChar = Console.ReadKey().KeyChar;
+            mapScale = (int)Char.GetNumericValue(mapScaleChar);
+            Console.ReadKey(true);
+
+            ScaleWindow();
         }
 
         static void ScaleWindow()
         {
-            Console.WindowWidth = map.GetLength(1) * mapScale + 2;
+            try
+            {
+                Console.WindowWidth = map.GetLength(1) * mapScale + 2;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Given value either not an integer or too large to scale to");
+                Console.ReadKey(true);
+                SetScale();
+                return;
+            }
+            DrawMap();
+            DrawBorder();
         }
 
         static void DrawMap()
         {
-            ScaleWindow();
+            Console.ForegroundColor = ConsoleColor.White;
 
             for (int i = 0; i < map.GetLength(0); i++)
             {
@@ -141,6 +159,178 @@ namespace rpgMap
             Console.WriteLine('╚');
             Console.SetCursorPosition(map.GetLength(1) * mapScale + 1, map.GetLength(0) * mapScale  + 1);
             Console.WriteLine('╝');
+        }
+
+        static void InitPlayer()
+        {
+            playerX = 7;
+            playerY = 6;
+            subX = 0;
+            subY = 0;
+            SetPlayerPos();
+        }
+
+        static void SetPlayerPos()
+        {
+            Console.SetCursorPosition((playerX + 1) * mapScale - subX, (playerY + 1) * mapScale - subY);
+            SetColour(playerX, playerY);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(player);
+        }
+
+        static void ClearInputBuffer()
+        {
+            while (Console.KeyAvailable)
+                Console.ReadKey(true);
+        }
+
+        static void InputLoop()
+        {
+            while (true)
+            {
+                char inputChar = Console.ReadKey(true).KeyChar;
+
+                switch (inputChar)
+                {
+                    case 'w':
+                        MoveUp();
+                        break;
+                    case 's':
+                        MoveDown();
+                        break;
+                    case 'a':
+                        MoveLeft();
+                        break;
+                    case 'd':
+                        MoveRight();
+                        break;
+                }
+            }
+        }
+
+        static void MoveUp()
+        {
+            FillOldPos();
+            subY++;
+            if (subY < 0 || subY >= mapScale)
+            {
+                if (playerY - 1 <= map.GetLength(1) * -1 || TestCollisionUp())
+                {
+                    subY--;
+                    SetPlayerPos();
+                    return;
+                }
+
+                subY = 0;
+                playerY--;
+            }
+            SetPlayerPos();
+        }
+        static void MoveDown()
+        {
+            FillOldPos();
+            subY--;
+            if (subY < 0 || subY >= mapScale)
+            {
+                if (playerY + 1 >= map.GetLength(0) || TestCollisionDown())
+                {
+                    subY++;
+                    SetPlayerPos();
+                    return;
+                }
+
+                subY = mapScale - 1;
+                playerY++;
+            }
+            SetPlayerPos();
+        }
+        static void MoveLeft()
+        {
+            FillOldPos();
+            subX++;
+            if (subX < 0 || subX >= mapScale)
+            {
+                if (playerX - 1 <= map.GetLength(0) * -1 || TestCollisionLeft())
+                {
+                    subX--;
+                    SetPlayerPos();
+                    return;
+                }
+
+                subX = 0;
+                playerX--;
+            }
+            SetPlayerPos();
+        }
+        static void MoveRight()
+        {
+            FillOldPos();
+            subX--;
+            if (subX < 0 || subX >= mapScale || TestCollisionRight())
+            {
+                if (playerX + 1 >= map.GetLength(1))
+                {
+                    subX++;
+                    SetPlayerPos();
+                    return;
+                }
+
+                subX = mapScale - 1;
+                playerX++;
+            }
+            SetPlayerPos();
+        }
+
+        static bool TestCollisionUp()
+        {
+            switch (map[playerY - 1, playerX])
+            {
+                case '^':
+                case '~':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        static bool TestCollisionDown()
+        {
+            switch (map[playerY + 1, playerX])
+            {
+                case '^':
+                case '~':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        static bool TestCollisionLeft()
+        {
+            switch (map[playerY, playerX - 1])
+            {
+                case '^':
+                case '~':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        static bool TestCollisionRight()
+        {
+            switch (map[playerY, playerX + 1])
+            {
+                case '^':
+                case '~':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        static void FillOldPos()
+        {
+            Console.SetCursorPosition((playerX + 1) * mapScale - subX, (playerY + 1) * mapScale - subY);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(map[playerY, playerX]);
         }
     }
 }
